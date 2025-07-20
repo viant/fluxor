73137145773a01ee1b51b970e58e14def9ac4dc4
adranwit <adrianwit@gmail.com>
2025-07-20 09:41:54 +0200

    added env expansion

--- model/types/context.go
+++ model/types/context.go
@@ -1,6 +1,21 @@
 package types
 
+import "context"
+
 type executionContextKey string
 
 // ExecutionContextKey execution context
 var ExecutionContextKey = executionContextKey("execution-context")
+
+// EnsureExecutionContext ensure
+func EnsureExecutionContext(ctx context.Context, pairs ...string) context.Context {
+	v := ctx.Value(ExecutionContextKey)
+	if v == nil {
+		ctx = context.WithValue(ctx, ExecutionContextKey, map[string]any{})
+	}
+	values := ctx.Value(ExecutionContextKey).(map[string]any)
+	for i := 0; i < len(pairs); i += 2 {
+		values[pairs[i]] = pairs[i+1]
+	}
+	return ctx
+}
